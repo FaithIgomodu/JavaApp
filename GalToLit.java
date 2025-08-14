16ebb39e844a74ec83c61a4aaa07d377ef86fc25
Faith Igomodu <168231295+FaithIgomodu@users.noreply.github.com>
2025-08-14 00:29:47 -0400

    Add files via upload
    initial commit

---
+++ GalToLit.java
@@ -0,0 +1,21 @@
+
+/*
+Author: Faith Igomodu
+Date: 8/12/2025
+Function converts gallons to liters
+ */
+
+
+public class GalToLit {
+    public static void main(String[] args){
+        //Variable definition
+        double gallons;
+        double liters ;
+
+        //Variable assignment
+        gallons = 10;
+        liters = gallons * 3.7854;
+
+        System.out.println(gallons + " "+ "gallons is" + " "+ liters +  " "+ "liters.");
+    }
+}
