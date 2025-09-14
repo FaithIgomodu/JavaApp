2bc796e0923a4d0285ec4876530f3b1ae193af31
Faith Igomodu <168231295+FaithIgomodu@users.noreply.github.com>
2025-09-13 23:45:59 -0400

    Update GCPercent.java
    Added comments

--- GCPercent.java
+++ GCPercent.java
@@ -5,81 +5,70 @@ File name: GCPercent
 Program author name: Faith Igomodu
 */
 
-import java.util.ArrayList;
-import java.util.List;
 
-//DNA class with id, seq properties
+
+//DNA class with id, seq properties to calculate GC content 
 class DNA {
+    //DNA sequence id 
     int id;
+    //DNA sequence 
    String seq;
 
 
-    //DNA class constructor
+    //DNA class constructor:
+    //Seq - DNA sequences 
+    //id - DNA sequence id 
     public DNA(String seq, int id) {
         this.id = id;
         this.seq = seq;
     }
 
-    //method for seq length
+    //Returns the length of DNA sequence 
     public int getSize(){
-
-        int  seqLen =0;
-
-        for(int i =0; i <seq.length(); i++){
-            seqLen++ ;
-        }
-
-        return seqLen;
+        return seq.length();
     }
 
-    //method for counting bases in seq
-    public int baseCount(char base){
-        this.seq = seq;
+    //Returns the count of a base by counting each base in the DNA sequence 
+    public int baseCount(char base){        
+        //Initialize counts to zero 
         int bCount =0;
-
+        //Count each base in DNA sequence  using a for loop 
         for(int i =0; i< seq.length(); i++){
+            //Convert each base to upper case for case insensativity 
             char bType = Character.toUpperCase(seq.charAt(i));
+            //Counting matches to the bases 
             if(bType == Character.toUpperCase(base)){
+                //Increase count for every match 
                 bCount++;
             }
         }
-
+        //Return base count 
         return bCount;
 
     }
 
-
-
 }
 
 
-
+//Main method that calculates GC content of a specific DNA sequence 
 public class GCPercent {
     public static void main(String[] args){
-        //declare  variables
-
+       
+        //Declare and initialize G and C counts to zero, for calculating GC percentage 
         int gCount = 0;
         int cCount = 0;
-
-
+        //Created a DNA object with sequence and id for GC calculation  
         DNA mDNA = new DNA("ATGCTTTACGTTTTACTGCCCTAAATCGATGCATGCATGACTGAAC", 2548365);
-
-
+        //baseCounts method is called and used to count number of G and C bases 
         gCount = mDNA.baseCount('G');
         cCount = mDNA.baseCount('C');
-
-
-
+        //Gets the length of DNA sequence 
        int dnaLen = mDNA.getSize();
-
-
+        //Calculate the GC percent of DNA sequence 
         double gcPercent = (gCount+cCount)*100.0 / dnaLen;
-        System.out.println("The GC Percent of DNA sequence is:");
+        //Display GC percentage to 2 decimal places and DNA sequence id 
+        System.out.println("The GC Percent of DNA sequence :" + mDNA.id);
         System.out.printf("%.2f%n" , gcPercent);
-
-
-
-
 
 
     }
