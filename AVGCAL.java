27d44ea8264911f18fd2b92258ba19ce8cc8520a
FaithIgomodu <figomodu@student.umgc.edu>
2025-09-17 11:11:07 -0400

    Initial commit

--- AVGCAL.java
+++ AVGCAL.java
@@ -1,24 +1,28 @@
 /* Week2 in-class exercise
    Name: Average_2.java
    Author: Faith Igomodu
-   Date: 8/18/2025
+   Date: 9/16/2025
+
+*/
+
+/* Week2 in-class exercise
+   Name: Average_2.java
+   Author: Faith Igomodu
+   Date: 9/16/2025
 
 */
 
 import java.util.Scanner;
 
-public class Main {
+
+public class AVGCAL {
     //Method calculates and returns the average grade
-    public static double AVGCAL(double... grade) {
-            //Variable declartion and instantiation
-        //sum of grades
+    public static double avgCal(double... grade) {
+
         double total = 0;
-        //average grade
         double avg_grade = 0;
-        //counts number of each grade
         double count = 0;
 
-        //Calcuates sum of grades and counts each grade
         for (double val : grade) {
             total += val;
             count++;
@@ -30,26 +34,31 @@ public class Main {
     }
 
 
-//Caller function that calls AVGCAL
+
 
     public static void main(String[] args) {
-
+        //Create scanner object to read user input
         Scanner scanner = new Scanner(System.in);
+        //Prompt the user to enter grades
         System.out.println(">>>>>>>>>>>>>>>>>>Average Grade Calculator<<<<<<<<<<<<<<<<<<<<<<<<<");
         System.out.println("Enter 8 Grades Separated by a Comma ");
 
+        //Read user input and split it into an array of strings
         String input = scanner.nextLine();
         String[] gradeS = input.split(",");
 
+        //Convert grade strings to doubles and store in an array
         double[] grades = new double[8];
         for (int i = 0; i < 8; i++) {
             grades[i] = Double.parseDouble(gradeS[i].trim());
         }
 
-        double results = AVGCAL(grades);
+        //Calculate the average grades
+        double results = avgCal(grades);
 
+        //Display the average grade
         System.out.printf("Average grade is: %.2f%n ", results);
-
+        //Determine and display pass / fail
         if (results >= 75.0) {
             System.out.println("You PASSED! ");
         } else if (results < 75.0) {
---
+++ out/production/java_biotechnology/README.md
@@ -0,0 +1,2 @@
+# java_biotechnology
+Collection of scripts from  Java for biotechnology course.
