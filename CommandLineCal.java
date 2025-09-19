c87161d5b9a5a5338c9e9f4e16ac90f5feee5e63
Faith Igomodu <168231295+FaithIgomodu@users.noreply.github.com>
2025-09-19 18:11:51 -0400

    Update and rename AVGCAL.java to CommandLineCal.java
    Refractor code for meet requirements

---
+++ CommandLineCal.java
@@ -0,0 +1,66 @@
+/* Week5 in-class exercise
+   Name: CommandLineCal
+   Author: Faith Igomodu
+   Date: 9/16/2025
+
+*/
+
+
+
+public class CommandLineCal{
+
+    public static void main(String[] args) {
+        //Check if an argument was passed
+        if(args.length == 0){
+            System.out.println("You did not input a value");
+        }
+        //Inform user of app use
+        System.out.println("This is a Command Line Calculator. " +
+                "Enter Any Number of grades and average grade " +
+                "will be returned"+"\n");
+
+        //Validate user input
+        for(int i =0; i <args.length; i++){
+            System.out.println("Grades Entered:" +  i+ ":" +args[i]);
+        }
+
+        //Convert String array to double array
+        double[] grades = new double[args.length];
+        for(int i = 0; i<args.length; i++){
+            grades[i]= Double.parseDouble(args[i]);
+        }
+
+
+        //variable instantiation
+        double total = 0;
+        double avg_Grade = 0;
+
+        //determine length of array grades
+        double lenGrade = grades.length;
+
+        //calculate sum of grades
+        for(double val: grades){
+            total += val;
+
+        }
+
+        //calculate average grade
+        avg_Grade = total/lenGrade;
+
+
+        //Display the average grade
+        System.out.printf("\n"+"Average grade is: %.2f%n ", avg_Grade);
+        //Determine and display pass / fail
+        if (avg_Grade >= 75.0) {
+            System.out.println("You PASSED! ");
+        } else if (avg_Grade < 75.0) {
+            System.out.println("You FAILED! ");
+        }
+
+        System.out.println("Author: Faith Igomodu");
+    }
+
+}
+
+
+
