12750c88fdffd8817f0f9551a8e9326a12f4a87f
Faith Igomodu <168231295+FaithIgomodu@users.noreply.github.com>
2025-10-24 15:36:21 -0400

    Rename FastaReader class to CountFastaFiles

--- CountFastaFiles.java
+++ CountFastaFiles.java
@@ -5,7 +5,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
-public class FastaReader {
+public class CountFastaFiles {
     public static void main(String[] args){
         String line ;
         int count =0;
